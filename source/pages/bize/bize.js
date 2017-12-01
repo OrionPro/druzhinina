5928f566f741db5f1873b2c2f9d79f57d4ebf884
orionpro <orion79@rambler.ru>
2017-12-01 13:23:41 +0200

    13 25 1.12.2017

--- source/pages/bize/bize.js
+++ source/pages/bize/bize.js
@@ -5,10 +5,29 @@ import './bize.sass';
 import '../../pages/bize/bize.pug'; //это для обновления страницы при hotreload - при npm build убрать
 //import animate from '../../js/animate_bize';
 
-console.log('in bize.js');
 
 $(document).ready( function() {
+	function dateTimer() {
+		const now = new Date().getTime();
+		const date1 = new Date(2017, 10, 30).getTime();
+		const date2 = new Date(2017, 11, 15).getTime();
+		const date3 = new Date(2017, 11, 30).getTime();
+		const date4 = new Date(2018, 0, 15).getTime();
 
+		if(date1 < now) {
+			$('.first-date .current').addClass('not-current');
+		}
+		if(date2 < now) {
+			$('.second-date .current').addClass('not-current');
+		}
+		if(date3 < now) {
+			$('.third-date .current').addClass('not-current');
+		}
+		if(date4 < now) {
+			$('.fourth-date .current').addClass('not-current');
+		}
+	}
+	dateTimer();
 });
 
 $(window).resize(function() {
